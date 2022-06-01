ce0bc599149e4b07cfa47b4218bb4560df018166
TecSoc <25559838+t-tecsoc@users.noreply.github.com>
2022-06-01 18:53:16 +0900

    うまくいかないので修正

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -7,8 +7,8 @@ LOGIN=$(cat <<-END
             "scope": "https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/service.management https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/cloud-platform openid https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/drive.file",
             "token_type": "Bearer",
             "id_token": "$ID_TOKEN",
-            "expiry_date":1654073398544,
-            "refresh_token": "$REFRESH_TOKEN",
+            "expiry_date": 1654073398544,
+            "refresh_token": "$REFRESH_TOKEN"
         },
         "oauth2ClientSettings": {
             "clientId": "$CLIENT_ID",
