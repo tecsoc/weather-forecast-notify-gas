ed9be9b11d5e0e47b868116cc930730fad7d342a
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2022-06-02 16:25:18 +0900

    リファクタリング

--- src/get-weather.js
+++ src/get-weather.js
@@ -1,105 +1,119 @@
-var spreadsheet;
-var sheet;
+let spreadsheet;
+let sheet;
 
-function getWeatherWeekly(){
-  let target_link = getWeatherLink("府県週間天気予報");
-  Logger.log(target_link);
-}
+class GetWeather {
 
-function getWeather(){
-  // XMLをパース
-  var target_link = getWeatherLink('府県天気予報（Ｒ１）');
-  Logger.log(target_link);
-  var xml = getXML(target_link);
-  // 名前空間を設定
-  var namespace = XmlService.getNamespace('http://xml.kishou.go.jp/jmaxml1/body/meteorology1/');
-  var TimeSeriesInfo = xml.getRootElement().getChild('Body', namespace).getChildren('MeteorologicalInfos', namespace)[0].getChildren('TimeSeriesInfo', namespace)[1];
-  var TimeDefines = TimeSeriesInfo.getChild('TimeDefines', namespace).getChildren('TimeDefine', namespace);
-  var items = TimeSeriesInfo.getChildren('Item', namespace);
-  // 降水確率が入ってる配列
-  var weatherArray = [];
-  // 降水確率が入ってる要素の配列
-  var target_elements;
-  for(var i = 0; i < items.length; i++) {
-    if (items[i].getChild('Area', namespace).getChildText('Code', namespace) === '130010'){
-      target_elements = items[i].getChild('Kind', namespace).getChild('Property', namespace).getChild('ProbabilityOfPrecipitationPart', namespace).getChildren();
-      break;
-    }
+  constructor () {
+    this.namespace = XmlService.getNamespace('http://xml.kishou.go.jp/jmaxml1/body/meteorology1/');
   }
-  for (var i = 0; i < target_elements.length; i++) {
-    // [yyyy/mm/dd, tt時からtt時まで, nn%] という形で配列に詰める
-    weatherArray.push(
-      [
-        // yyyy/mm/dd 形式に置換
-        TimeDefines[i].getChildText('DateTime', namespace).replace(/([0-9]{4})-([0-9]{2})-([0-9]{2}).+/m, '$1/$2/$3'),
-        // tt時からtt時まで
-        TimeDefines[i].getChildText('Name', namespace).replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-          // 正規表現で全角を半角に変換
-          return String.fromCharCode(s.charCodeAt(0) - 65248);
-        }),
-        // 降水確率
-        target_elements[i].getText() + "%"
-      ]
-    );
+
+  getXML(url){
+    let options = {
+      muteHttpExceptions: true
+    };
+    let xmlStr = UrlFetchApp.fetch(url, options);
+    return XmlService.parse(xmlStr);
+  //  return XmlService.parse(UrlFetchApp.fetch(url)).getContentText();
   }
-  Logger.log(weatherArray);
-  //.replace(/\s+/g, "");
-  setSheet('降水確率');
-  // 値があるセルをクリアする
-  sheet.clearContents();
-  // スプレッドシートに書き込む
-  sheet.getRange(1, 1, weatherArray.length, weatherArray[0].length).setValues(weatherArray);
-  
-  target_link = getWeatherLink('府県天気概況');
-  Logger.log(target_link);
-  xml = getXML(target_link);
-  var weather_overview = xml.getRootElement().getChild('Body', namespace).getChild('Comment', namespace).getChildText('Text', namespace);
-  weather_overview = weather_overview.replace(/ |　|^\n/gm, "");
-  weather_overview = weather_overview.replace(/。伊豆諸島.+。/g, "。");
-  weather_overview = weather_overview.replace(/\n\【関東甲信地方\】(\S|\s)+/gm, "");
-  setSheet('天気概況');
-  sheet.getRange(1, 1).setValue(weather_overview);
-}
 
-function getXML(url){
-  var options = {
-    muteHttpExceptions:true
-  };
-  var xml_str = UrlFetchApp.fetch(url, options);
-  return XmlService.parse(xml_str);
-//  return XmlService.parse(UrlFetchApp.fetch(url)).getContentText();
-}
-
-// 府県天気予報
-function getWeatherLink(type, place="気象庁") {
-  var xml = getXML('https://www.data.jma.go.jp/developer/xml/feed/regular_l.xml');
-  // 名前空間の取得
-  var atom = XmlService.getNamespace('http://www.w3.org/2005/Atom');
-  // entry要素のlistを取得
-  var entries = xml.getRootElement().getChildren("entry", atom);
-  for (var i = 0; i < entries.length; i++) {
-    // タイトルが違ったらcontinue
-    if (entries[i].getChildText('title', atom) === type){
-      // 東京かどうか
-      if (entries[i].getChild('author', atom).getChildText('name', atom).indexOf(place) !== -1){
-        return entries[i].getChild('link', atom).getAttribute('href').getValue();
-      }
-    }
-    /*if (entries[i].getChildText('title', atom) === type) {
-      if (type === '府県天気予報'){
-        if (entries[i].getChildText('content', atom).indexOf(place) != -1) {
+  getWeatherXmlLink(deliveryType = "府県天気予報", author = "気象庁") {
+    let xml = this.getXML('https://www.data.jma.go.jp/developer/xml/feed/regular_l.xml');
+    // 名前空間の取得
+    let atom = XmlService.getNamespace("http://www.w3.org/2005/Atom");
+    // entry要素のlistを取得
+    let entries = xml.getRootElement().getChildren("entry", atom);
+    for (let i = 0; i < entries.length; i++) {
+      // タイトルが違ったらcontinue
+      if (entries[i].getChildText('title', atom).indexOf(deliveryType) !== -1){
+        // 東京かどうか
+        if (entries[i].getChild('author', atom).getChildText('name', atom).indexOf(author) !== -1){
           return entries[i].getChild('link', atom).getAttribute('href').getValue();
         }
-      } else{
-        if ()
       }
-    }*/
+    }
+  }
+
+
+  getRainfallProbability() {
+
+    let targetLink = this.getWeatherXmlLink("府県天気予報");
+    Logger.log(targetLink);
+
+    let xml = this.getXML(targetLink);
+
+    let timeSeriesInfo = xml.getRootElement().getChild('Body', this.namespace).getChildren('MeteorologicalInfos', this.namespace)[0].getChildren('TimeSeriesInfo', this.namespace).slice(-1)[0];
+    
+    let timeDefines = timeSeriesInfo.getChild('TimeDefines', this.namespace).getChildren('TimeDefine', this.namespace);
+    let items = timeSeriesInfo.getChildren('Item', this.namespace);
+
+    let rainfallProbabilityArray = [];
+    // 降水確率が入ってる要素の配列
+    let elementsInRainfallProbability;
+    for(let i = 0; items.length; i++) {
+      if (items[i].getChild("Area", this.namespace).getChildText("Code", this.namespace) === "130010"){
+        elementsInRainfallProbability = items[i].getChild("Kind", this.namespace).getChild("Property", this.namespace).getChild("ProbabilityOfPrecipitationPart", this.namespace).getChildren();
+        break;
+      }
+    }
+    for (let i = 0; i < elementsInRainfallProbability.length; i++) {
+      // [yyyy/mm/dd, tt時からtt時まで, nn%] という形で配列に詰める
+      rainfallProbabilityArray.push(
+        [
+          // yyyy/mm/dd 形式に置換
+          timeDefines[i].getChildText('DateTime', this.namespace).replace(/([0-9]{4})-([0-9]{2})-([0-9]{2}).+/m, '$1/$2/$3'),
+          // tt時からtt時まで
+          timeDefines[i].getChildText('Name', this.namespace).replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+            // 正規表現で全角を半角に変換
+            return String.fromCharCode(s.charCodeAt(0) - 65248);
+          }),
+          // 降水確率
+          elementsInRainfallProbability[i].getText() + "%"
+        ]
+      );
+    }
+    return rainfallProbabilityArray;
+  }
+
+  getWeatherOverview() {
+    let targetLink = this.getWeatherXmlLink('府県天気概況');
+    Logger.log(targetLink);
+    let xml = this.getXML(targetLink);
+    let weatherOverview = xml.getRootElement().getChild('Body', this.namespace).getChild('Comment', this.namespace).getChildText('Text', this.namespace);
+    weatherOverview = weatherOverview.replace(/ |　|^\n/gm, "");
+    weatherOverview = weatherOverview.replace(/。伊豆諸島.+。/g, "。");
+    weatherOverview = weatherOverview.replace(/\n\【関東甲信地方\】(\S|\s)+/gm, "");
+    return weatherOverview;
+  }
+
+  setWeatherOverviewToSheet(str) {
+    setSheet('天気概況');
+    sheet.getRange(1, 1).setValue(str);
+  }
+
+  setRainfallProbabilityToSheet(rainfallProbabilityArray) {
+    setSheet('降水確率');
+    // 値があるセルをクリアする
+    sheet.clearContents();
+    // スプレッドシートに書き込む
+    sheet.getRange(1, 1, rainfallProbabilityArray.length, rainfallProbabilityArray[0].length).setValues(rainfallProbabilityArray);
   }
 }
+
+
+function getWeather(){
+  let GetWeatherHandler = new GetWeather();
+  let rainfallProbabilityArray = GetWeatherHandler.getRainfallProbability();
+  Logger.log(rainfallProbabilityArray);
+  GetWeatherHandler.setRainfallProbabilityToSheet(rainfallProbabilityArray);
+  let weatherOverview = GetWeatherHandler.getWeatherOverview();
+  Logger.log(weatherOverview);
+  GetWeatherHandler.setWeatherOverviewToSheet(weatherOverview);
+}
+
 
 
 function setSheet(sheet_name){
-  var sheet_id = "1Z2pynN7cWB0CAawy8e_qZkIlJ7WqPE1Voi0hBvWCELA"
+  var sheet_id = "1Z2pynN7cWB0CAawy8e_qZkIlJ7WqPE1Voi0hBvWCELA";
 
   // シートの取得
   spreadsheet = SpreadsheetApp.openById(sheet_id);
