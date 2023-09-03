b68814020b5e442e67892636a89b65b5abee2c58
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-09-03 21:45:52 +0900

    bug fix

--- src/setting.js
+++ src/setting.js
@@ -4,8 +4,8 @@ const Start_Col_Num = 3;
 const Setting_Length = 8;
 
 getUseridList = () => sheet.getRange(Start_Col_Num, User_Row_Num, sheet.getLastRow()).getValues();
-getUserIndex = (userId) => getUseridList().findIndex(item => item[0] === userId) + Start_Col_Num;
-getWeekdayRangeByUser = (index) => sheet.getRange(index, Setting_Row_Num, 1, Setting_Length);
+getUserIndex = (userId) => getUseridList().findIndex(item => item[0] === userId);
+getWeekdayRangeByUser = (index) => sheet.getRange(index + Start_Col_Num, Setting_Row_Num, 1, Setting_Length);
 
 const setSetting = (userId, settings) => {
   setSheet('天気配信管理');
