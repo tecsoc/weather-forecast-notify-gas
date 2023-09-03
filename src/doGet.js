cefffddba80de9c6dab7255a04873a63d58d96f2
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-09-03 21:39:21 +0900

    fix typo

--- src/doGet.js
+++ src/doGet.js
@@ -2,7 +2,7 @@ const doGet = (e) => {
   const response = {};
   const userId = e.parameter.userId;
   if (e.parameter.type === 'updateSetting') {
-    const result = setSetting(userId, r.parameters.settings);
+    const result = setSetting(userId, e.parameters.settings);
     response.result = result;
   } else {
     const settings = getSetting(userId);
