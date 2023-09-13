2f60d8f6c9c153065f0e7f6156aaa62f2ace6821
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-09-13 09:33:29 +0900

    fix message: 'must be non-empty text' error

--- src/spreadSheet.js
+++ src/spreadSheet.js
@@ -234,7 +234,7 @@ class SpreadSheet {
       const weekdayStr = weekdayArray[dateObject.getDay()];
       row[0] = row[0].replace(/[0-9]{4}\//,"");
       row[0] += `(${weekdayStr})`;
-      if (row[2]) row[2] += "%";
+      row[2] += row[2] ? "%" : '-';
       return row.join(" ");
     });
     return weeklyWeatherForecastList;
